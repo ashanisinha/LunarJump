7141ad3e70b9bc0d2d9c329c500c6b31d7de5eec
ashanisinha <ashani.sinha@gmail.com>
2023-01-14 14:57:52 -0800

    fixed random spawning error!

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject platformPrefab;
-    public int platformCount = 300; 
+   // public int platformCount = 300; 
     // Start is called before the first frame update
     void Start()
     {
+        
         Vector3 spawnPosition = new Vector3();
 
-        for (int i = 0; i < platformCount; i++) {
+        for (int i = 0; i < 300; i++) {
             spawnPosition.y += Random.Range(.5f, 2f);
             spawnPosition.x = Random.Range(-5f, 5f);
             Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+            
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
